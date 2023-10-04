1f6a61c0a0beb316f8f541e7ca28fd1e74a4a2dc
michalgalant0 <michal_galant@wp.pl>
2023-10-04 12:21:38 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.fotoradar {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.fotoradar to javafx.fxml;
+    exports com.example.fotoradar;
+}
