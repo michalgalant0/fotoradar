dbc4630773c1d4ecfb822af2d598f0872f492122
paweljabloniec12 <paweljabloniec12@gmail.com>
2023-10-18 23:04:14 +0200

    Obsługa przycisku usuwania wybranej kolekcji. (nie kaskadowo)

--- src/main/java/com/example/fotoradar/controllers/CollectionViewController.java
+++ src/main/java/com/example/fotoradar/controllers/CollectionViewController.java
@@ -1,16 +1,21 @@
 package com.example.fotoradar.controllers;
 
 import com.example.fotoradar.SwitchScene;
+import com.example.fotoradar.databaseOperations.CollectibleOperations;
+import com.example.fotoradar.databaseOperations.CollectionOperations;
 import com.example.fotoradar.models.Collectible;
 import com.example.fotoradar.models.Collection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 @Getter @Setter
 public class CollectionViewController {
@@ -19,12 +24,21 @@ public class CollectionViewController {
     public Button backToCollectionsButton;
     public Button addCollectibleButton;
     public Button editCollectionButton;
+    public Button deleteCollectionButton;
 
     private Collection collection;
 
     @FXML
-    public void initialize() {
+    public void initialize() throws SQLException {
         titleLabel.setText("kolekcje/ "+collection.getTitle());
+        fillListView();
+    }
+
+    private void fillListView() throws SQLException {
+        ArrayList<Collectible> collectibles = new CollectibleOperations().getAllCollectibles(collection.getId());
+
+        for (Collectible collectible : collectibles)
+            collectiblesListView.getItems().add(collectible);
     }
 
     @FXML
@@ -40,8 +54,27 @@ public class CollectionViewController {
         new SwitchScene(event, "add_collectible_view", controller);
     }
 
-    public void editCollection(ActionEvent event) throws IOException {
-        //tmp
-        new SwitchScene(event, "edit_collection_view");
+
+    public void editCollection(ActionEvent event) throws IOException, SQLException {
+        // Tworzenie kontrolera dla widoku edycji kolekcji i przekazywanie parametrów kolekcji
+        EditCollectionViewController controller = new EditCollectionViewController();
+        controller.setCollection(getCollection());
+
+        // Przenoszenie do widoku kolekcji z przekazaną kolekcją
+        new SwitchScene(event, "edit_collection_view", controller);
     }
+
+    @FXML
+    private void deleteCollection(ActionEvent event) throws IOException, SQLException {
+        CollectionOperations collectionOperations = new CollectionOperations();
+        boolean isDeleted = collectionOperations.deleteCollection(collection.getId());
+
+        if (isDeleted) {
+            // Po usunięciu -> powrót do widoku listy kolekcji
+            new SwitchScene(event, "collections-view");
+        } else {
+            // Obsługa przypadku, gdy nie udało się usunąć kolekcji
+        }
+    }
+
 }
