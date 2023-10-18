bb3ae379d02e600cfc14fcb9617c43f80c46912b
paweljabloniec12 <paweljabloniec12@gmail.com>
2023-10-18 23:04:14 +0200

    Obsługa przycisku usuwania wybranej kolekcji. (nie kaskadowo)

--- src/main/java/com/example/fotoradar/controllers/CollectionViewController.java
+++ src/main/java/com/example/fotoradar/controllers/CollectionViewController.java
@@ -2,6 +2,7 @@ package com.example.fotoradar.controllers;
 
 import com.example.fotoradar.SwitchScene;
 import com.example.fotoradar.databaseOperations.CollectibleOperations;
+import com.example.fotoradar.databaseOperations.CollectionOperations;
 import com.example.fotoradar.models.Collectible;
 import com.example.fotoradar.models.Collection;
 import javafx.event.ActionEvent;
@@ -23,6 +24,7 @@ public class CollectionViewController {
     public Button backToCollectionsButton;
     public Button addCollectibleButton;
     public Button editCollectionButton;
+    public Button deleteCollectionButton;
 
     private Collection collection;
 
@@ -52,6 +54,7 @@ public class CollectionViewController {
         new SwitchScene(event, "add_collectible_view", controller);
     }
 
+
     public void editCollection(ActionEvent event) throws IOException, SQLException {
         // Tworzenie kontrolera dla widoku edycji kolekcji i przekazywanie parametrów kolekcji
         EditCollectionViewController controller = new EditCollectionViewController();
@@ -60,4 +63,18 @@ public class CollectionViewController {
         // Przenoszenie do widoku kolekcji z przekazaną kolekcją
         new SwitchScene(event, "edit_collection_view", controller);
     }
+
+    @FXML
+    private void deleteCollection(ActionEvent event) throws IOException, SQLException {
+        CollectionOperations collectionOperations = new CollectionOperations();
+        boolean isDeleted = collectionOperations.deleteCollection(collection.getId());
+
+        if (isDeleted) {
+            // Po usunięciu -> powrót do widoku listy kolekcji
+            new SwitchScene(event, "collections-view");
+        } else {
+            // Obsługa przypadku, gdy nie udało się usunąć kolekcji
+        }
+    }
+
 }
