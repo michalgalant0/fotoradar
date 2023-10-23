6285f6ab794efaa2c8f90452d08e6f8651811970
michalgalant0 <michal_galant@wp.pl>
2023-10-23 11:15:45 +0200

    Utworzone komponenty formularzowe (CollectionForm, CollectibleForm, SegmentForm, VersionForm).

--- src/main/java/com/example/fotoradar/components/CollectibleForm.java
+++ src/main/java/com/example/fotoradar/components/CollectibleForm.java
@@ -1,4 +1,25 @@
 package com.example.fotoradar.components;
 
-public class CollectibleForm {
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class CollectibleForm extends AnchorPane {
+    @FXML
+    public TextField titleTextField;
+    @FXML
+    public DatePicker startDatePicker;
+    @FXML
+    public DatePicker finishDatePicker;
+    @FXML
+    public TextArea descriptionTextArea;
+    @FXML
+    public ComboBox statusComboBox;
+
+    public CollectibleForm() {}
+
+    public void initialize() {}
 }
