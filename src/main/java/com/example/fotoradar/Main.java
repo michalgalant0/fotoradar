7b77ba7100b9a64d6e27cd204c856bbaf7153e72
michalgalant0 <michal_galant@wp.pl>
2023-10-24 12:35:35 +0200

    Utworzone okienka formularz kolekcji i formularz obiektu razem z metodą displayWindow, ktora otworzy nowe okienko w oparciu o przekazaną nazwę okienka.

--- src/main/java/com/example/fotoradar/Main.java
+++ src/main/java/com/example/fotoradar/Main.java
@@ -11,7 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         // using fxml loader for layout loading
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("collections-view.fxml"));
+//        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("collections-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("windows/TEST-WINDOWS.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("fotoradar");
         stage.setScene(scene);
--- src/main/java/com/example/fotoradar/components/CollectibleForm.java
+++ src/main/java/com/example/fotoradar/components/CollectibleForm.java
@@ -1,11 +1,15 @@
 package com.example.fotoradar.components;
 
+import com.example.fotoradar.Main;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class CollectibleForm extends AnchorPane {
     @FXML
@@ -19,7 +23,16 @@ public class CollectibleForm extends AnchorPane {
     @FXML
     public ComboBox statusComboBox;
 
-    public CollectibleForm() {}
+    public CollectibleForm() {
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("components/CollectibleForm.fxml"));
+        loader.setRoot(this);
+        loader.setController(this);
+
+        try {
+            loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }}
 
     public void initialize() {}
 }
