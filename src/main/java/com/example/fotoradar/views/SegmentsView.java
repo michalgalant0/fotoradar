9c8eefa173f7fbbc63bed10c04321f9ae42886c2
michalgalant0 <michal_galant@wp.pl>
2023-10-25 12:51:34 +0200

    Utworzony widok SegmentsView.

--- src/main/java/com/example/fotoradar/views/SegmentsView.java
+++ src/main/java/com/example/fotoradar/views/SegmentsView.java
@@ -1,4 +1,25 @@
 package com.example.fotoradar.views;
 
+import com.example.fotoradar.components.MiniGalleryComponent;
+import com.example.fotoradar.components.SegmentFormComponent;
+import javafx.fxml.FXML;
+
 public class SegmentsView {
+    @FXML
+    public MiniGalleryComponent miniGalleryComponent;
+    @FXML
+    public SegmentFormComponent segmentFormComponent;
+
+    @FXML
+    private void saveSegment(){}
+    @FXML
+    private void deleteSegment(){}
+    @FXML
+    private void addSketch(){}
+    @FXML
+    private void addPhoto(){}
+    @FXML
+    private void addSegments(){}
+    @FXML
+    private void backToCollection(){}
 }
