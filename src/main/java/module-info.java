e0ec199485c21b6ad6e07d67eb5da21ae6dfabc1
michalgalant0 <michal_galant@wp.pl>
2023-10-25 13:58:46 +0200

    Implementacja otwarcia okienka formularz kolekcji na przycisku DODAJ KOLEKCJĘ na widoku CollectionsView. Dodane pliki workflow.txt oraz buttons.txt - pomocnicze.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,8 +10,6 @@ module com.example.fotoradar {
 
     opens com.example.fotoradar to javafx.fxml;
     exports com.example.fotoradar;
-    exports com.example.fotoradar.controllers;
-    opens com.example.fotoradar.controllers to javafx.fxml;
     exports com.example.fotoradar.databaseOperations;
     opens com.example.fotoradar.databaseOperations to javafx.fxml;
 
