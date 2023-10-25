7a998061c1d1ed1a7a7151a6457a679644f087a3
paweljabloniec12 <paweljabloniec12@gmail.com>
2023-10-25 13:05:38 +0200

    Utworzenie versionView.

--- src/main/java/com/example/fotoradar/views/VersionView.java
+++ src/main/java/com/example/fotoradar/views/VersionView.java
@@ -1,4 +1,37 @@
 package com.example.fotoradar.views;
 
+import com.example.fotoradar.components.VersionFormComponent;
+import com.example.fotoradar.components.MiniGalleryComponent;
+import javafx.fxml.FXML;
+
 public class VersionView {
+    @FXML
+    public MiniGalleryComponent miniGalleryComponent;
+    @FXML
+    public VersionFormComponent versionFormComponent;
+
+    @FXML
+    private void saveVersion() {
+        System.out.println("zapis wersji");
+    }
+
+    @FXML
+    private void addPhotos() {
+        System.out.println("dodawanie zdjęć");
+    }
+
+    @FXML
+    private void manageTeams() {
+        System.out.println("przejscie do zespołów");
+    }
+
+    @FXML
+    private void removeVersion() {
+        System.out.println("usuwanie wersji");
+    }
+
+    @FXML
+    private void backToSegments() {
+        System.out.println("powrot do segmentów");
+    }
 }
