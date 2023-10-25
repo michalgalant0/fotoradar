6593b80717a7cc1f4558113153f5f308a912c838
michalgalant0 <michal_galant@wp.pl>
2023-10-25 07:48:12 +0200

    Utworzone okienko z podglądem zdjęć.

--- src/main/java/com/example/fotoradar/windows/ImageViewerWindow.java
+++ src/main/java/com/example/fotoradar/windows/ImageViewerWindow.java
@@ -1,4 +1,17 @@
 package com.example.fotoradar.windows;
 
-public class ImageViewerWindow {
+import com.example.fotoradar.components.ImageViewer;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import lombok.Setter;
+
+public class ImageViewerWindow implements Window {
+    @FXML
+    private ImageViewer imageViewer;
+    @Setter
+    private Stage dialogStage;
+
+    public void deletePhoto() {
+        System.out.println("usuniecie biezacego zdjecia");
+    }
 }
