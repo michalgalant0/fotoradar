681da8ed234ad262e8ed70309425a0be12d7a07f
voiteckheir <wojceichdziecic@gmail.com>
2023-11-22 08:19:49 +0100

    still not show

--- src/main/java/com/example/fotoradar/components/TeamComponent.java
+++ src/main/java/com/example/fotoradar/components/TeamComponent.java
@@ -20,52 +20,29 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class TeamComponent extends AnchorPane{
-    @FXML
-    public Label nameLabel;
-    @FXML
-    public Label descriptionLabel;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import lombok.Setter;
 
+public class TeamComponent extends VBox {
     @FXML
-    private VBox teamsContainer;
+    private Label nameLabel;
     @FXML
-    private ScrollPane scrollPane;
+    private TextArea descriptionTextArea;
 
     @Setter
     private Team team;
 
-    private ArrayList<Team> teamy;
-
+    public TeamComponent() {
+        // FXMLLoader nie jest potrzebny, ponieważ używamy VBox jako korzenia
+    }
 
     public void initialize() {
-
+        if (team != null) {
+            nameLabel.setText(team.getName());
+            descriptionTextArea.setText(team.getDescription());
+        }
     }
-
-    public TeamComponent() throws IOException {
-        FXMLLoader loader = new FXMLLoader(Main.class.getResource("components/TeamComponent.fxml"));
-        loader.setRoot(this);
-        loader.setController(this);
-
-        loader.load();
-    }
-    public void setNameLabel(String name) {
-        nameLabel.setText(name);
-    }
-
-    public void setDescriptionLabel(String status) {
-        descriptionLabel.setText(status);
-    }
-
-    public void fillTeamsVBox() throws IOException, SQLException {
-       /* for (Team team : teamy) {
-            teamRowComponent teamsRow = new CollectionRowComponent();
-            teamsRow.setCollectible(team);
-            teamsRow.setNameLabel(teams.getTitle());
-            teamsRow.setDescription(team.getDescription());
-
-
-            collectiblesContainer.getChildren().add(collectionRow);
-        }*/
-    }
-
 }
--- src/main/java/com/example/fotoradar/components/TeamsComponent.java
+++ src/main/java/com/example/fotoradar/components/TeamsComponent.java
@@ -1,6 +1,7 @@
 package com.example.fotoradar.components;
 
 import com.example.fotoradar.Main;
+import com.example.fotoradar.databaseOperations.TeamOperations;
 import com.example.fotoradar.models.Collectible;
 import com.example.fotoradar.models.Team;
 import javafx.fxml.FXMLLoader;
@@ -10,16 +11,18 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import lombok.Setter;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class TeamsComponent extends AnchorPane {
     @FXML
-    private HBox teamsContainer;
+    private VBox teamsContainer;
 
     @Setter
     private ArrayList<Team> teams;
@@ -33,18 +36,22 @@ public class TeamsComponent extends AnchorPane {
     }
 
     public void fillTeamsHBox() throws IOException {
-        for (Team team : teams) {
-            // utworzenie komponentu zespołu
-            TeamComponent teamComponent =
-                    new TeamComponent();
-            // przekazanie zespołu do komponentu
-            teamComponent.setTeam(team);
-            // ustawienie nazwy zespołu
-            teamComponent.setNameLabel(team.getName());
-            // ustawienie opisu zespołu
-            teamComponent.setDescriptionLabel(team.getDescription());
+        try {
+            TeamOperations teamOperations = new TeamOperations();
+            List<Team> teamsList = teamOperations.getAllTeams();
 
-            teamsContainer.getChildren().add(teamComponent);
+            for (Team team : teamsList) {
+                // utworzenie komponentu zespołu
+                TeamComponent teamComponent = new TeamComponent();
+                // przekazanie zespołu do komponentu
+                teamComponent.setTeam(team);
+
+                // wypełnienie komponentu danymi zespołu
+                teamComponent.initialize();
+                teamsContainer.getChildren().add(teamComponent);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
