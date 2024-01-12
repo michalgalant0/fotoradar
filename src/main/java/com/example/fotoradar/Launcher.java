cc888541ef46b327eb80c9d44a6b70f1c11e2a32
michalgalant0 <michal_galant@wp.pl>
2024-01-12 08:33:13 +0100

    Zmiany na potrzeby budowania aplikacji

---
+++ src/main/java/com/example/fotoradar/Launcher.java
@@ -0,0 +1,8 @@
+package com.example.fotoradar;
+
+// https://stackoverflow.com/questions/54806788/javafx-cant-build-artifact-fxdeploy-is-not-available-in-this-jdk
+public class Launcher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
